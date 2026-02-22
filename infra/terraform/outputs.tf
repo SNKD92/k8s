529b6ac73db7e6517b17cf4b663aa154826d8fa2
Denis <denis@joinmassive.com>
2026-02-21 20:32:44 -0500

    Enable automatic GKE deployment

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -5,3 +5,7 @@ output "cluster_name" {
 output "cluster_zone" {
   value = google_container_cluster.small.location
 }
+
+output "github_actions_service_account_email" {
+  value = google_service_account.github_actions.email
+}
